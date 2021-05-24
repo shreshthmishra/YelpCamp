8a9d6475574472dcd2994f06a96a4a937c49fe62
shreshthmishra <shreshth.mishra.cd.eee19@itbhu.ac.in>
2021-05-24 14:11:11 +0530

    add CRUD functionalities

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const Campground = require('./models/campground');
+const methodOverride = require('method-override');
 
 // copied from mongoose documentations
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -20,6 +21,7 @@ db.once('open', function () {
 const app = express();
 
 app.use(express.urlencoded({ extended: true })); // It parses incoming requests
+app.use(methodOverride('_method'));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views')) // so that I can access views even if I don't start my app from YelpCamp directory
@@ -33,9 +35,36 @@ app.get('/campgrounds', async function (req, res) {
     res.render('campgrounds/index', { campgrounds });
 });
 
+app.post('/campgrounds', async function (req, res) {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`campgrounds/${campground._id}`);
+});
+
+app.get('/campgrounds/new', function (req, res) {
+    res.render('campgrounds/new');
+});
+
+app.get('/campgrounds/:id/edit', async function (req, res) {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', { campground });
+})
+
+app.put('/campgrounds/:id', async function (req, res) {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, req.body.campground);
+    res.redirect(`/campgrounds/${campground._id}`);
+});
+
 app.get('/campgrounds/:id', async function (req, res) {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', { campground });
+});
+
+app.delete('/campgrounds/:id', async function (req, res) {
+    const id = req.params.id;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
 });
 
 app.listen(3000, function (req, res) {
