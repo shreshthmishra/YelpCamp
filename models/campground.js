92e8dc665f69e6c0cbe56a5a1b1a7b38cfb34410
shreshthmishra <shreshth.mishra.cd.eee19@itbhu.ac.in>
2021-06-01 17:03:23 +0530

    add review form

--- models/campground.js
+++ models/campground.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
+const Review = require('./review');
 
 const CampgroundSchema = new mongoose.Schema({
     title: String,
     image: String,
     price: Number,
     description: String,
-    location: String
+    location: String,
     reviews: [
         {
             type: mongoose.Schema.Types.ObjectId,
