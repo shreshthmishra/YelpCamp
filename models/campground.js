757e3a2c8869a9148226c94a61350cda0ea55e1e
shreshthmishra <shreshth.mishra.cd.eee19@itbhu.ac.in>
2021-06-05 18:36:14 +0530

    add campground delete Middleware

--- models/campground.js
+++ models/campground.js
@@ -15,4 +15,14 @@ const CampgroundSchema = new mongoose.Schema({
     ]
 });
 
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+})
+
 module.exports = mongoose.model('Campground', CampgroundSchema);
