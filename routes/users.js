6cd26a34ffca1054e45c102690a06903d94e3644
shreshthmishra <shreshth.mishra.cd.eee19@itbhu.ac.in>
2021-06-24 13:04:33 +0530

    add login functionality

--- routes/users.js
+++ routes/users.js
@@ -26,7 +26,13 @@ router.post('/register', catchAsync(async (req, res, next) => {
 
 router.get('/login', (req, res) => {
     res.render('users/login');
-})
+});
 
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    req.flash('success', 'welcome back!');
+    const redirectUrl = req.session.returnTo || '/campgrounds';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
+});
 
-module.exports = router
+module.exports = router;
